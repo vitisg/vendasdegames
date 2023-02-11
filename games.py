591080a673a74b1dfc1ff02838f9aeaeeae8eb18
vitisg <125102978+vitisg@users.noreply.github.com>
2023-02-11 16:37:08 -0300

    games.py

---
+++ games.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:/Users/lenovo/Downloads/vgsales.csv")
+
+df = df.dropna()
+
+df['Year'] = df['Year'].astype(int)
+
+df = df.rename(columns = {'Name': 'Nome', 'Platform': 'Plataforma', 'Year': 'Ano de lançamento', 'Genre': 'Genêro', 'Publisher': 'Publicadora', 'NA_Sales': 'Vendas na américa do sul',
+       'EU_Sales': 'Vendas nos EUA', 'JP_Sales': 'Vendas no Japão', 'Other_Sales': 'Vendas em outros lugares', 'Global_Sales': 'Venda global'})
+
+
+
+sns.countplot(df['Genêro'])
+fig = plt.gcf()
+fig.set_size_inches(15,10)
+plt.title('Type')
