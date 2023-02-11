ae66f9e0877611dea85d4e13445159e11b0c2da3
vitisg <125102978+vitisg@users.noreply.github.com>
2023-02-11 17:35:51 -0300

    Update games.py

--- games.py
+++ games.py
@@ -28,8 +28,8 @@ with col1:
 with col2:
        st.header("Análise das vendas por genêro e localidae")
        nomes = list(df['Publicadora'].unique())
-       publicaora = st.sidebar.selectbox('Qual publicadora de vendas?', nomes)
-       fig = px.histogram(df, x='Genêro', y= publicaora)
+       publicadora = st.sidebar.selectbox('Qual publicadora de vendas?', nomes)
+       fig = px.histogram(df, x= publicadora)
        fig.update_layout(bargap=0.2)
        st.plotly_chart(fig, use_container_width=True)
 with col3:
