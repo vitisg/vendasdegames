154cc412b3eaba670990e39641fb444ec8aaa5b5
vitisg <125102978+vitisg@users.noreply.github.com>
2023-02-11 17:31:07 -0300

    Update games.py

--- games.py
+++ games.py
@@ -11,20 +11,30 @@ df['Year'] = df['Year'].astype(int)
 df = df.rename(columns = {'Name': 'Nome', 'Platform': 'Plataforma', 'Year': 'Ano de lançamento', 'Genre': 'Genêro', 'Publisher': 'Publicadora', 'NA_Sales': 'Vendas na américa do sul',
        'EU_Sales': 'Vendas nos EUA', 'JP_Sales': 'Vendas no Japão', 'Other_Sales': 'Vendas em outros lugares', 'Global_Sales': 'Venda global'})
 
-
-
-
-#ANALISE DE VENDAS
 st.title('DADOS DE VENDAS GLOBAL DE JOGOS DE VIDEOGAME')
 st.write('Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de local para mostrar as vendas por geêro no grafico. Utilize o menu lateral para alterar a amostragem')
 
-locais = ['Vendas na américa do sul', 'Vendas nos EUA',
-       'Vendas no Japão', 'Vendas em outros lugares', 'Venda global']
-local = st.sidebar.selectbox('Qual o local das vendas?', locais)
+col1, col2, col3 = st.columns(3)
 
-fig = px.histogram(df, x='Genêro', y= local)
-fig.update_layout(bargap=0.2)
-st.plotly_chart(fig, use_container_width=True)
+with col1:
+       st.header("Análise das vendas por genêro e localidae")
+       locais = ['Vendas na américa do sul', 'Vendas nos EUA',
+              'Vendas no Japão', 'Vendas em outros lugares', 'Venda global']
+       local = st.sidebar.selectbox('Qual o local das vendas?', locais)
+       fig = px.histogram(df, x='Genêro', y= local)
+       fig.update_layout(bargap=0.2)
+       st.plotly_chart(fig, use_container_width=True)
+
+with col2:
+   st.header("Análise das vendas por genêro e localidae")
+       nomes = list(df['Publicadora'].unique())
+       publicaora = st.sidebar.selectbox('Qual publicadora de vendas?', nomes)
+       fig = px.histogram(df, x='Genêro', y= nomes)
+       fig.update_layout(bargap=0.2)
+       st.plotly_chart(fig, use_container_width=True)
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
 
 
 
