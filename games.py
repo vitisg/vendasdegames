9004aa80655137504e217727fa2b4b6c349cf204
vitisg <125102978+vitisg@users.noreply.github.com>
2023-02-11 16:55:28 -0300

    Update games.py

--- games.py
+++ games.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 df = pd.read_csv("https://raw.githubusercontent.com/vitisg/vendasdegames/main/vgsales.csv")
 
@@ -14,10 +11,9 @@ df['Year'] = df['Year'].astype(int)
 df = df.rename(columns = {'Name': 'Nome', 'Platform': 'Plataforma', 'Year': 'Ano de lançamento', 'Genre': 'Genêro', 'Publisher': 'Publicadora', 'NA_Sales': 'Vendas na américa do sul',
        'EU_Sales': 'Vendas nos EUA', 'JP_Sales': 'Vendas no Japão', 'Other_Sales': 'Vendas em outros lugares', 'Global_Sales': 'Venda global'})
 
-
-
-sns.countplot(df['Genêro'])
-fig = plt.gcf()
-fig.set_size_inches(15,10)
-plt.title('Type')
-
+st.title('DADOS DE VENDAS GLOBAL DE JOGOS DE VIDEOGAME')
+st.write('Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de informação para mostrar o grafico. Utilize o menu lateral para alterar a amostragem')
+fig = px.histogram(df, x="Genêro")
+fig.update_layout(bargap=0.2)
+st.plotly_chart(fig, use_container_width=True)
+st.caption('Os dados foram obtidos a partir o site: https://www.kaggle.com/datasets/thedevastator/global-video-game-sales')
